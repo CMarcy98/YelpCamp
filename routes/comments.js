c23e23ed112dad7498aa204264184b4c6a751902
Christian Marcy <christian.marcy@gmail.com>
2018-05-18 17:43:16 -0400

    Update and Delete Comment

--- routes/comments.js
+++ routes/comments.js
@@ -33,6 +33,42 @@ router.post("/", isLoggedIn, (req,res) => {
 	})
 });
 
+
+// Gets the edit form for an existing comment
+router.get("/:comment_id/edit", (req, res) => {
+	Comment.findById(req.params.comment_id, (err, foundComment) => {
+		if(err) {
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", {camp_id: req.params.id, comment: foundComment});
+		}
+	});
+});
+
+
+// Updates an existing comment
+router.put("/:comment_id", (req, res) => {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err) => {
+		if(err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id)
+		}
+	});
+});
+
+
+// Delete an existing comment
+router.delete("/:comment_id", (req, res) => {
+	Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+		if(err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id)
+		}
+	})
+});
+
 // Checks to see if user is logged in
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()) {
